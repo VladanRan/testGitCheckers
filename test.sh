cdb40d36428258aa761131f3471b3b96a655cc2f
Vladan Randjelovic <vladan.randjelovic@mapp.com>
2020-03-05 22:34:46 +0100

    Test4

---
+++ test.sh
@@ -0,0 +1,30 @@
+set -eo pipefail
+
+EXIT_STATUS=0
+
+. ./scripts/check_secrets.sh
+check_secrets
+
+if [[ "$have_secrets" == true ]]; then
+    (xcodebuild \
+      -workspace /Users/vladan.randjelovic/Desktop/testGitCheckers/OClintExampleApp/OClintExampleApp.xcworkspace \
+      -scheme OClintExampleApp \
+      -sdk iphonesimulator \
+      -destination 'platform=iOS Simulator,name=iPhone 11' \
+      build \
+      test \
+      ONLY_ACTIVE_ARCH=YES \
+      | xcpretty) || EXIT_STATUS=$?
+else
+    # Skip running tests if GoogleService-Info.plist's weren't decoded.
+    (xcodebuild \
+      -workspace /Users/vladan.randjelovic/Desktop/testGitCheckers/OClintExampleApp/OClintExampleApp.xcworkspace \
+      -scheme OClintExampleApp \
+      -sdk iphonesimulator \
+      -destination 'platform=iOS Simulator,name=iPhone 11' \
+      build \
+      ONLY_ACTIVE_ARCH=YES \
+      | xcpretty) || EXIT_STATUS=$?
+fi
+
+  exit $EXIT_STATUS
